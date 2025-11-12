8e0309653b89287a8f0b2c2a4fd684658de47b09
Angelmacario <angelica.parizotto@outlook.com>
2025-11-11 21:08:47 -0300

    Finalizando o projeto forca : )

--- main.py
+++ main.py
@@ -5,7 +5,40 @@ from utils import limpar_tela, exibir_titulo
 
 # Função principal do jogo
 def main():
-    palavras = escolher_palavra()
+    palavra = escolher_palavra()
     letras_certas = set() # definir conjunto vazio das palavras
-    letras_erradas = set() # 
+    letras_erradas = set()  
     tentativas = 6
+
+    #cria o loop do jogo
+    while tentativas > 0:
+        limpar_tela()
+        exibir_titulo()
+        print(mostrar_palavra(palavra, letras_certas))
+        print(f"\n❌ Letras erradas: {', '.join(letras_erradas)}")
+        print(f"🔄 Tentativas restantes: {tentativas}")
+        letra = input("\nDigite uma letra: ").lower() #deixar tudo minúsculo
+        if len(letra) != 1 or not letra.isalpha(): #len = verifica se entrada é um unico caracter | isalpha = verifica se é numero ou qlqr outro caracter
+            print("Por favor, digite apenas uma letra válida.")
+            input("Pressione Enter para continuar...")
+            continue
+        if letra in letras_certas or letra in letras_erradas:
+            print("Você já tentou essa letra. Tente outra.")
+            input("Pressione Enter para continuar...")
+            continue
+        if verificar_letra(letra, palavra, letras_certas, letras_erradas):
+            print("✅ Boa! Letra Correta!")
+        else:
+            print("❌ Letra Errada!")
+            tentativas -= 1
+        if verificar_vitoria(palavra, letras_certas):
+            limpar_tela()
+            exibir_titulo()
+            print(f"🥳 Parabéns você acertou a palavra: {palavra}")
+            break
+    else:
+        print("😒 Fim de Jogo! A palavra era: {palavra}")
+
+#permite que o arquivo python
+if __name__ == "__main__":
+    main()
